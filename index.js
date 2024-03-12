f2d3adb56e20276fa0a528816c7692758412f9a6
Yuvashree R <91588494+YuvashreeRchan@users.noreply.github.com>
2024-03-12 20:46:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-
+document.addEventListener("DOMContentLoaded", function () {
 const playButton = document.getElementById("play-btn");
 playButton.onclick = () =>
 {
     window.location.href ="game.html";
+}
 }
 
